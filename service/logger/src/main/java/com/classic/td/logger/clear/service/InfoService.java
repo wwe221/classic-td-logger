d7f20bd72eca22b397183583f61628513556affa
wwe221 <wwe22123@gmail.com>
2023-12-11 09:27:57 +0900

    Modify: Towerdata isNotEmpty

--- service/logger/src/main/java/com/classic/td/logger/clear/service/InfoService.java
+++ service/logger/src/main/java/com/classic/td/logger/clear/service/InfoService.java
@@ -4,6 +4,7 @@ import com.classic.td.logger.clear.domain.ClearInfo;
 import com.classic.td.logger.clear.domain.tower.TowerDamage;
 import com.classic.td.logger.clear.repository.InfoRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,26 +32,29 @@ public class InfoService {
             boolean flag = info.isFlag();
             countService.createClearCount(info.getUserId(), info.isFlag(), info.getTime());
             var towerData = info.getTowerData();
-            var split1 = towerData.replaceAll("\\(", "")
-                    .replaceAll("\\)", "")
-                    .split(";");
-            //이름1: 인덱스(int) : 데미지(int64) 로 타워데미지 테이블에 넣기
-            List<TowerDamage> towerDamageList = new ArrayList<>();
-            for (int i = 0; i < split1.length; i++) {
-                try {
-                    var towerInfo = split1[i].split(":");
-                    var name = towerInfo[0];
-                    var index = Integer.parseInt(towerInfo[1]);
-                    var damage = Long.parseLong(towerInfo[2]);
-                    towerDamageList.add(createTowerDamage(version, flag, info.getTime(), index, damage));
-                } catch (ArrayIndexOutOfBoundsException ignored) {}
+            if (StringUtils.isNotEmpty(towerData)) {
+                var split1 = towerData.replaceAll("\\(", "")
+                        .replaceAll("\\)", "")
+                        .split(";");
+                //이름1: 인덱스(int) : 데미지(int64) 로 타워데미지 테이블에 넣기
+                List<TowerDamage> towerDamageList = new ArrayList<>();
+                for (int i = 0; i < split1.length; i++) {
+                    try {
+                        var towerInfo = split1[i].split(":");
+                        var name = towerInfo[0];
+                        var index = Integer.parseInt(towerInfo[1]);
+                        var damage = Long.parseLong(towerInfo[2]);
+                        towerDamageList.add(createTowerDamage(version, flag, info.getTime(), index, damage));
+                    } catch (ArrayIndexOutOfBoundsException ignored) {
+                    }
 
+                }
+                var towerCounts = towerDamageList.stream()
+                        .collect(Collectors.groupingBy(TowerDamage::getTowerIndex, Collectors.counting()));
+                towerService.saveTowerDamages(towerDamageList);
+                if (!towerCounts.isEmpty())
+                    towerService.saveTowerCount(towerCounts, version, flag);
             }
-            var towerCounts = towerDamageList.stream()
-                    .collect(Collectors.groupingBy(TowerDamage::getTowerIndex, Collectors.counting()));
-            towerService.saveTowerDamages(towerDamageList);
-            if (!towerCounts.isEmpty())
-                towerService.saveTowerCount(towerCounts, version, flag);
         });
         return repository.saveAll(clearInfos);
     }
